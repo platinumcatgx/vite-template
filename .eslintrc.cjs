43288cf34d05f9195f22264fb7c0820a8a039f8f
platinumcatgx <773441322@qq.com>
2022-08-05 20:51:38 +0800

    'init'

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/vue3-essential",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "parser": "@typescript-eslint/parser",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue",
+        "@typescript-eslint",
+        'plugin:prettier/recommended'
+    ],
+    "rules": {
+      'no-undef': 0
+    }
+}
